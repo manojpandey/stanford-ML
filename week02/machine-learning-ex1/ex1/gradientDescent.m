a80cc70564baa871953cf5c3c42a053c55414815
Manoj Pandey <manojpandey1996@gmail.com>
2015-12-21 04:33:03 +0530

    solutions for ex1

--- week02/machine-learning-ex1/ex1/gradientDescent.m
+++ week02/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,15 +16,17 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    h = ((X * theta)-y)';
+    
+    temp1 = theta(1) - alpha * (1/m) * h * X(:, 1);
+    temp2 = theta(2) - alpha * (1/m) * h * X(:, 2);
+    
+    theta(1) = temp1;
+    theta(2) = temp2;
 
     % ============================================================
-
+    
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
